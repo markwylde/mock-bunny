165659443d5ae63c175e9019eac8ecd65b64b570
Mark Wylde <me@markwylde.com>
2024-10-08 22:44:22 +0100

    fix: use the environment variable values

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import createServer from './createServer.js';
 
 const server = createServer({
-  storageZoneName: 'my-storage',
-  accessKey: 'my-access-key',
-  uploadFolder: './uploaded-files'
+  storageZoneName: process.env.STORAGE_ZONE_NAME,
+  accessKey: process.env.ACCESS_KEY,
+  uploadFolder: process.env.UPLOAD_FOLDER,
+  publicRead: process.env.PUBLIC_READ === 'true',
 });
 
 server.listen(3000, () => {
